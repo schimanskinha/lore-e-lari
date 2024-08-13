edaa6787a9f20c4583341d0d4699475dd43d0ccf
schimanskinha <129195331+schimanskinha@users.noreply.github.com>
2024-08-13 08:21:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+<script src="main.js"></script>
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultados= document.querySelector(".texto-resultados");
+
+const perguntas = [
+{
+    enunciado: "base líquida ou em pó?",
+    alternativas
+                
+    
+    : [
+        {
+            texto: "líquida?",
+            afirmacao: "# "
+        },
+        {
+            texto: "em pó ?",
+            afirmacao: "#."
+        }
+    ]
+},
+{
+    enunciado: "Máscara de cílios à prova d'água ou normal?",
+    alternativas: [
+        {
+            texto: "À prova d'água",
+            afirmacao: "# "
+        },
+        {
+            texto: "Normal",
+            afirmacao: "#"
+        }
+    ]
+},
+{
+            
+            enunciado: "Delineador em gel ou líquido?",
+    alternativas: [
+
+
+        {
+            texto: "À prova d'água",
+            afirmacao: "# "
+        },
+        {
+            texto: "Normal",
+            afirmacao: "#"
+                
+    
+    
+        
+        }
+    ]
+},
+
+]
